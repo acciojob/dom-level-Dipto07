c1aa4e8eaa605e9d733a64c822f37fb7d4f80b42
Dipto Neogi <81651700+Dipto07@users.noreply.github.com>
2024-04-30 14:48:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+
+	const targetElement = document.getElementById('level');
+    
+    let domLevel = 0;
+    
+    let currentElement = targetElement;
+	
+    while (currentElement !== document.documentElement) {
+        currentElement = currentElement.parentNode;
+        domLevel++;
+    }
+    
+    alert('The level of the element is: ' + domLevel);
+});
